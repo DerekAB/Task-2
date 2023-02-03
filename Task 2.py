d95cbeba4ed438961a274c1418746f76d3100b2e
DerekAB <123401143+DerekAB@users.noreply.github.com>
2023-02-03 18:12:47 +0000

    start

--- Task 2.py
+++ Task 2.py
@@ -1,1 +1,10 @@
-print("test")
+#Name:                  Task 2.py
+#Author:                Derek Baker
+#Date Created:          03-02-2023
+#Date Last Modified:    03-02-2023
+#
+#Purpose:
+#
+#The purpose of this program is to ask the user if they want to calculate the area of a circle, rectangle, or triangle. 
+#The program will ask for the length of each side, and then present the area and the perimeter to the user.
+

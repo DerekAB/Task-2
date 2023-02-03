07e154673284158a6138a2577399628d306354c3
DerekAB <123401143+DerekAB@users.noreply.github.com>
2023-02-03 18:23:43 +0000

    using code from task 1

--- Task 2.py
+++ Task 2.py
@@ -10,3 +10,39 @@
 #Most of this code will be copied from Task 1
 
 
+import math
+answer = input("Please enter if you would like to calculate the area of a triangle, " + \
+               "circle, or rectangle. Or if you would like to exit: ") #Get the user's choice 
+
+while not(answer.strip().upper() == "TRIANGLE" or answer.strip().upper() == "CIRCLE" or answer.strip().upper() == "RECTANGLE" or answer.strip().upper() == "EXIT"):
+    answer = input("Please enter a valid answer: ")                                        #if the user enters an invalid answer, it will loop until they do 
+
+if answer.strip().upper() == "TRIANGLE":
+    triangleLength = float(input("Please enter the length of triangle in centimetres: "))           #Have the user enter the triangle length
+    
+    triangleArea = (triangleLength * triangleLength) / 2                            #Calculating the area and the perimeter
+    trianglePerimeter = triangleLength * 3
+    
+    print("The area of the triangle is: " + str(triangleArea) + "cm^2.")
+    print("The perimeter of the triangle is " + str(trianglePerimeter) + "cm.")
+    
+if answer.strip().upper() == "RECTANGLE":
+    rectangleLength = float(input("Please enter the length of the rectangle: "))                #Have the user enter the rectangle length
+    
+    rectangleArea = rectangleLength * 2                                                #Calculate the reactangle area and perimeter
+    rectanglePerimeter = rectangleLength * 4 
+    
+    print("The area of the rectangle is " + str(rectangleArea) + "cm^2.")
+    print("The perimeter of the rectangle is " + str(rectanglePerimeter) + "cm.")
+    
+if answer.strip().upper() == "CIRCLE":
+    circleRadius = float(input("Please enter the radius of the circle: "))                        #Have the user enter the circle radius
+    
+    circleArea = math.pi * (circleRadius * circleRadius)
+    circlePerimeter = 2 * math.pi * circleRadius
+    
+    print("The area of the circle is " + str(circleArea) + "cm^2.")                     #Calculate the area and perimeter of the circle
+    print("The perimeter of the circle is " + str(circlePerimeter) + "cm.")
+    
+if answer.strip().upper() == "EXIT":                    #If the user enters "exit", the program will end
+    print("OK, goodbye.")
